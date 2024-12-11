b214abb19328de5c98671109ac22a39854d23eb8
wangonya <kwangonya@gmail.com>
2024-12-11 17:46:35 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -4,24 +4,33 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
-func handleConnection(c net.Conn) {
-	c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+func handleConnection(conn net.Conn) {
+	req := make([]byte, 1024)
+	conn.Read(req)
+	if !strings.HasPrefix(string(req), "GET / HTTP/1.1") {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		conn.Close()
+	}
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
+		fmt.Println(err)
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	c, err := l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	handleConnection(c)
+	handleConnection(conn)
 }
