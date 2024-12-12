1c9a48a15008ecdfea431dbb8831f3aafa60e295
wangonya <kwangonya@gmail.com>
2024-12-12 06:34:13 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -4,17 +4,32 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"regexp"
 	"strings"
 )
 
+func extractEchoString(content string) string {
+	re := regexp.MustCompile(`GET /echo/(.*) `)
+	match := re.FindStringSubmatch(content)
+	return match[1]
+}
 func handleConnection(conn net.Conn) {
 	req := make([]byte, 1024)
 	conn.Read(req)
-	if !strings.HasPrefix(string(req), "GET / HTTP/1.1") {
+	content := string(req)
+
+	if !strings.HasPrefix(content, "GET / HTTP/1.1") && !strings.HasPrefix(content, "GET /echo/") { // TODO: do this better with regex
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 		conn.Close()
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
+	if strings.HasPrefix(content, "GET / HTTP/1.1") {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		echoString := extractEchoString(content)
+		response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoString), echoString)
+		conn.Write([]byte(response))
+	}
 }
 
 func main() {
